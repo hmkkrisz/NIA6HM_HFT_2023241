176e66e59d7e6af7a3b5e78fdf526018f8e5a6d4
homok <homok@DESKTOP-MM4PDD6>
2023-11-13 17:57:34 +0100

    Add project files.

---
+++ NIA6HM_HFT_2023241.Logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NIA6HM_HFT_2023241.Logic
+{
+    public class Class1
+    {
+    }
+}
---
+++ NIA6HM_HFT_2023241.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NIA6HM_HFT_2023241.Repository
+{
+    public class Class1
+    {
+    }
+}
