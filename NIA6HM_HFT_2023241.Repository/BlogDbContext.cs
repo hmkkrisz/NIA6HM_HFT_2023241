851031efbff392444519775aae140cdb00ae6150
Homoki Krisztofer <homokikrisz16@stud.uni-obuda.hu>
2023-11-15 15:06:17 +0100

    DbContext létrehozása

---
+++ NIA6HM_HFT_2023241.Repository/BlogDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NIA6HM_HFT_2023241.Models;
+using System;
+
+namespace NIA6HM_HFT_2023241.Repository
+{
+    public class BlogDbContext : DbContext
+    {
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
--- NIA6HM_HFT_2023241.Repository/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace NIA6HM_HFT_2023241.Repository
-{
-    public class Class1
-    {
-    }
-}
