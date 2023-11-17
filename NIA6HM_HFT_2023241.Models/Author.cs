ece3cec584022159a011cfed1a56a92a7d7c27b9
Homoki Krisztofer <homokikrisz16@stud.uni-obuda.hu>
2023-11-17 10:04:45 +0100

    Attributomok

--- NIA6HM_HFT_2023241.Models/Author.cs
+++ NIA6HM_HFT_2023241.Models/Author.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace NIA6HM_HFT_2023241.Models
 {
     public class Author
-    {       
-        [Key] public int AuthorId { get; set; }
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int AuthorId { get; set; }
+
+        [Required]
         public string Name { get; set; }
         public virtual ICollection<Article> Articles { get; set; }
     }
