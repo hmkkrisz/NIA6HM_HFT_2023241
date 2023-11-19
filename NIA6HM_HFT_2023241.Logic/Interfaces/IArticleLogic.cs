e720bc236b6ef4a587fe190b616286ab4caeb459
Homoki Krisztofer <homokikrisz16@stud.uni-obuda.hu>
2023-11-19 09:53:58 +0100

    CRUD metódusok

--- NIA6HM_HFT_2023241.Logic/Interfaces/IArticleLogic.cs
+++ NIA6HM_HFT_2023241.Logic/Interfaces/IArticleLogic.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using NIA6HM_HFT_2023241.Models;
 
 namespace NIA6HM_HFT_2023241.Logic
