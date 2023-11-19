17f3acb5792b186f6c084113c8e4565fadf7fdca
Homoki Krisztofer <homokikrisz16@stud.uni-obuda.hu>
2023-11-19 09:54:50 +0100

    CRUD metódusok

--- NIA6HM_HFT_2023241.Logic/Interfaces/IAuthorLogic.cs
+++ NIA6HM_HFT_2023241.Logic/Interfaces/IAuthorLogic.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
+using NIA6HM_HFT_2023241.Models;
 
 namespace NIA6HM_HFT_2023241.Logic
 {
     public interface IAuthorLogic
     {
+        void Create(Author item);
+        void Delete(int id);
+        Author Read(int id);
+        IQueryable<Author> ReadAll();
+        void Update(Author item);
     }
 }
