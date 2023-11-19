43eb2a852fecfaf5577e0e02c252868e1bddf45b
Homoki Krisztofer <homokikrisz16@stud.uni-obuda.hu>
2023-11-19 09:55:29 +0100

    CRUD metódusok

--- NIA6HM_HFT_2023241.Logic/Interfaces/ICommentLogic.cs
+++ NIA6HM_HFT_2023241.Logic/Interfaces/ICommentLogic.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
+using NIA6HM_HFT_2023241.Models;
 
 namespace NIA6HM_HFT_2023241.Logic
 {
     public interface ICommentLogic
     {
+        void Create(Comment item);
+        void Delete(int id);
+        Comment Read(int id);
+        IQueryable<Comment> ReadAll();
+        void Update(Comment item);
     }
 }
