e83422ce736a620f73c28350a34f366e3996bc58
Homoki Krisztofer <homokikrisz16@stud.uni-obuda.hu>
2023-11-19 09:59:43 +0100

    Interfacek implementalasa

--- NIA6HM_HFT_2023241.Logic/Classes/ArticleLogic.cs
+++ NIA6HM_HFT_2023241.Logic/Classes/ArticleLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NIA6HM_HFT_2023241.Models;
+using NIA6HM_HFT_2023241.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,36 @@ using System.Threading.Tasks;
 
 namespace NIA6HM_HFT_2023241.Logic
 {
-    public class ArticleLogic
+    public class ArticleLogic : IArticleLogic
     {
+        IRepository<Article> repo;
+        public ArticleLogic(IRepository<Article> repo)
+        {
+            this.repo = repo;
+        }
+        public void Create(Article item)
+        {
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Article Read(int id)
+        {
+            return this.repo.Read(id);
+        }
+
+        public IQueryable<Article> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Article item)
+        {
+            this.repo.Update(item);
+        }
     }
 }
