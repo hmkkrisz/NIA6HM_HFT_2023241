e0dc8b69e7bb2f2ed50ca7b11d001df7f8808576
Homoki Krisztofer <homokikrisz16@stud.uni-obuda.hu>
2023-11-19 09:53:12 +0100

    CRUD metódusok

--- NIA6HM_HFT_2023241.Logic/Interfaces/IArticleLogic.cs
+++ NIA6HM_HFT_2023241.Logic/Interfaces/IArticleLogic.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NIA6HM_HFT_2023241.Models;
 
 namespace NIA6HM_HFT_2023241.Logic
 {
     public interface IArticleLogic
     {
+        void Create(Article item);
+        void Delete(int id);
+        Article Read(int id);
+        IQueryable<Article> ReadAll();
+        void Update(Article item);
     }
 }
