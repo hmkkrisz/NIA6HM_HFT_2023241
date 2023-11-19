b37d09d9bad09adab588512fad8e5220ab94c271
Homoki Krisztofer <homokikrisz16@stud.uni-obuda.hu>
2023-11-19 10:03:19 +0100

    Interface implementalasa

--- NIA6HM_HFT_2023241.Logic/Classes/CommentLogic.cs
+++ NIA6HM_HFT_2023241.Logic/Classes/CommentLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NIA6HM_HFT_2023241.Models;
+using NIA6HM_HFT_2023241.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,36 @@ using System.Threading.Tasks;
 
 namespace NIA6HM_HFT_2023241.Logic
 {
-    public class CommentLogic
+    public class CommentLogic : ICommentLogic
     {
+        IRepository<Comment> repo;
+        public CommentLogic(IRepository<Comment> repo)
+        {
+            this.repo = repo;
+        }
+        public void Create(Comment item)
+        {
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Comment Read(int id)
+        {
+            return this.repo.Read(id);
+        }
+
+        public IQueryable<Comment> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Comment item)
+        {
+            this.repo.Update(item);
+        }
     }
 }
