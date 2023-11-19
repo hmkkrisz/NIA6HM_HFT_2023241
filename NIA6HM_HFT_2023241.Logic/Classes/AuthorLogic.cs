271a224ef224f81e9833e4e961dd478f65b4a49c
Homoki Krisztofer <homokikrisz16@stud.uni-obuda.hu>
2023-11-19 10:05:29 +0100

    interface implementalasa

--- NIA6HM_HFT_2023241.Logic/Classes/AuthorLogic.cs
+++ NIA6HM_HFT_2023241.Logic/Classes/AuthorLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NIA6HM_HFT_2023241.Models;
+using NIA6HM_HFT_2023241.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,36 @@ using System.Threading.Tasks;
 
 namespace NIA6HM_HFT_2023241.Logic
 {
-    public class AuthorLogic
+    public class AuthorLogic : IAuthorLogic
     {
+        IRepository<Author> repo;
+        public AuthorLogic(IRepository<Author> repo)
+        {
+            this.repo = repo;
+        }
+        public void Create(Author item)
+        {
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Author Read(int id)
+        {
+            return this.repo.Read(id);
+        }
+
+        public IQueryable<Author> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Author item)
+        {
+            this.repo.Update(item);
+        }
     }
 }
